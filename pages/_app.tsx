bb57679b3685f576d8994688699e4e89eb4a8a9c
ivankrzak <krzak.ivan@gmail.com>
2022-03-12 11:00:14 +0100

    Add Chakra Ui package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+)
 
 export default MyApp
